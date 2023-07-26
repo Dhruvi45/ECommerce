adf63293ce2eaa200cbd6c68197a256fa45f9858
Dhruvi45 <dhruvijoshi45@gmail.com>
2023-07-26 10:41:35 +0530

    remove footer

--- src/pages/HomePage/HomePageComponents/index.js
+++ src/pages/HomePage/HomePageComponents/index.js
@@ -1,5 +1,4 @@
 export { NavBar } from "./NavBar/NavBar";
-export { Footer } from "./Footer/Footer";
 export { CategoryCard } from "./CategoryCard/CategoryCard";
 export { Banner } from "./Banner/Banner";
 export { TrendingCard } from "./TrendingCard/TrendingCard";
